a5ae1beb03b31f9d069ba847602e6c9362fde453
Tyler Wilbers <tyler.wilbers@intel.com>
2023-06-09 14:09:59 -0700

    fixed conflict

--- explainer/tests/test_attributions.py
+++ explainer/tests/test_attributions.py
@@ -21,8 +21,10 @@
 import pytest
 from deepdiff import DeepDiff
 import numpy as np
+import pandas as pd
 import scipy as sp
 import transformers
+import torch
 torch.manual_seed(0)
 ### library to be tested ###
 from explainer import attributions
@@ -38,95 +40,6 @@ def test_deep_explainer(custom_pyt_CNN):
     deViz = attributions.deep_explainer(model, X_test[:2], X_test[2:4], class_names)
     assert isinstance(deViz, attributions.attributions.DeepExplainer) 
     deViz.visualize()
-
-@pytest.mark.parametrize("custom_CNN", ['custom_pyt_CNN', 'custom_tf_CNN'])
-def test_GradientExplainer(custom_CNN, request):
-    '''
-    Test every way to instantiate GradientExplainer and PTGradientExplainer objects
-    '''
-    model, X_test, class_names, y_test = request.getfixturevalue(custom_CNN)
-    if isinstance(X_test, torch.Tensor) == True:
-        geViz = attributions.GradientExplainer(model, X_test[:2], X_test[2:4], class_names)
-        assert isinstance(geViz, attributions.PTGradientExplainer) 
-        geViz = attributions.PTGradientExplainer(model, X_test[:2], X_test[2:4], class_names)
-        assert isinstance(geViz, attributions.PTGradientExplainer) 
-        geViz = attributions.gradient_explainer(model, X_test[:2], X_test[2:4], class_names)
-        assert isinstance(geViz, attributions.PTGradientExplainer) 
-    else:
-        geViz = attributions.GradientExplainer(model, X_test[:2], X_test[2:4], class_names)
-        assert isinstance(geViz, attributions.GradientExplainer) 
-        geViz = attributions.gradient_explainer(model, X_test[:2], X_test[2:4], class_names)
-        assert isinstance(geViz, attributions.GradientExplainer) 
-    geViz.visualize()
-
-@pytest.mark.parametrize("custom_CNN", ['custom_pyt_CNN', 'custom_tf_CNN'])
-def test_GradientExplainer_one_image(custom_CNN, request):
-    '''
-    Test edge case of one image input (with 1 and 2 background images and 1 and 2 ranked outputs)
-    '''
-    model, X_test, class_names, y_test = request.getfixturevalue(custom_CNN)
-    geViz = attributions.gradient_explainer(model, X_test[:1], X_test[2:3], class_names, 1)
-    geViz = attributions.gradient_explainer(model, X_test[:1], X_test[2:3], class_names, 2)
-    geViz = attributions.gradient_explainer(model, X_test[:1], X_test[2:4], class_names, 1)
-    geViz = attributions.gradient_explainer(model, X_test[:1], X_test[2:4], class_names, 2)
-    geViz.visualize()
-
-def test_partition_image(tf_resnet50, dog_cat_image, imagenet_class_names):
-    from tensorflow.keras.applications.resnet50 import preprocess_input
-    def f(X):
-        tmp = X.copy()
-        preprocess_input(tmp)
-        return tf_resnet50(tmp)
-
-    image = np.expand_dims(dog_cat_image, axis=0) 
-
-    # test generator class manually
-    pe = attributions.PartitionExplainer('image', f, imagenet_class_names, image[0].shape)
-    pe.run_explainer(image)
-    assert isinstance(pe, attributions.PartitionImageExplainer)
-    pe.visualize()
-
-    # test explicit class
-    pe = attributions.PartitionImageExplainer('image', f, imagenet_class_names,image[0].shape)
-    pe.run_explainer(image)
-    assert isinstance(pe, attributions.PartitionImageExplainer)
-    pe.visualize()
-
-    # test module function
-    pe = attributions.partition_image_explainer(f, imagenet_class_names, image)
-    assert isinstance(pe, attributions.PartitionImageExplainer)
-    pe.visualize()
-
-def test_partition_text():
-    labels = ['sadness', 'joy', 'love', 'anger', 'fear', 'surprise']
-    tokenizer = transformers.AutoTokenizer.from_pretrained("nateraw/bert-base-uncased-emotion", use_fast=True)
-    model = transformers.AutoModelForSequenceClassification.from_pretrained("nateraw/bert-base-uncased-emotion")
-
-    def f(x):
-        tv = torch.tensor([tokenizer.encode(v, padding='max_length', max_length=128, truncation=True) for v in x])
-        attention_mask = (tv!=0).type(torch.int64)
-        outputs = model(tv,attention_mask=attention_mask)[0].detach().cpu().numpy()
-        scores = (np.exp(outputs).T / np.exp(outputs).sum(-1)).T
-        val = sp.special.logit(scores)
-        return val
-    
-    # test generator class manually
-    pe = attributions.PartitionExplainer('text', f, labels, tokenizer)
-    pe.run_explainer(['i didnt feel humiliated'])
-    assert isinstance(pe, attributions.PartitionTextExplainer)
-    pe.visualize()
-
-    # test explicit class 
-    pe = attributions.PartitionTextExplainer('text', f, labels, tokenizer)
-    pe.run_explainer(['i didnt feel humiliated'])
-    assert isinstance(pe, attributions.PartitionTextExplainer)
-    pe.visualize()
-
-    # test module function
-    pe = attributions.partition_text_explainer(f, labels, ['i didnt feel humiliated'], tokenizer)
-    assert isinstance(pe, attributions.PartitionTextExplainer)
-    pe.visualize()
-
 
 def test_shap_waterwall_plot():
     kwargs = dict(expected_value=.5,
